544171f79e7a74a3090ab2ff672cecb94a25a856
moha-dev94 <ennadifima@gmail.com>
2026-01-05 16:33:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
   app.run(debug=True)
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return "contact.html"
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
